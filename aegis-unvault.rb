ddc582eedd8b12e333da3648e23625288cbca1cf
Léon <git.staleness589@passmail.com>
2026-02-24 17:52:46 +0100

    Update description for AegisUnvault class

--- aegis-unvault.rb
+++ aegis-unvault.rb
@@ -1,5 +1,5 @@
 class AegisUnvault < Formula
-  desc "Decrypt and view Aegis vaults in your terminal"
+  desc "nice tool to decrypt a 2fa aegis vault without the app, built with ratatui"
   homepage "https://github.com/leonpwd/aegis-unvault"
   url "https://github.com/leonpwd/aegis-unvault/releases/download/v1.0.0/aegis-unvault-macos.tar.gz"
   sha256 "f16a88c5e79784c0172caddbb60301c3f52fe3ed3181ea5154e17a1618341640"
